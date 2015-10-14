b881b7c2f05323ecebad641274a4da8a754a7a0a
zonika <zchodosh@conncoll.edu>
2015-10-14 11:23:26 -0400

    done

--- format_number.rb
+++ format_number.rb
@@ -1,3 +1,9 @@
 def separate_with_comma(num)
-  # code goes here
+  n = num.to_s.reverse
+  if n.length > 3
+    a = n.split(/(\d{3})/)
+    a.delete('')
+    n = a.join(',')
+  end
+  n.reverse
 end
